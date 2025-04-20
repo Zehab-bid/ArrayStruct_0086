a03b182268813d4c9d403919e09e9f01cc773996
Zehab-bid <zehabidha152gmail.com>
2025-04-20 15:32:01 +0700

    end struct

--- Struct.cpp
+++ Struct.cpp
@@ -22,6 +22,15 @@ int main()
          cout << "Masukan nama ke " << i + 1 << " = ";
          cin >> nama[i];
      }
+
+     cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama ke " << i + 1 << " = " << nama[i] << endl;
+    }
  
 
 
