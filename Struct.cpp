53d6dee34d3d6a93955268a493a2d5d1c71a7662
Zehab-bid <zehabidha152gmail.com>
2025-04-20 15:30:51 +0700

    proges 3

--- Struct.cpp
+++ Struct.cpp
@@ -16,7 +16,13 @@ int main()
     nilai[2] = 100;
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
 
-
+     // mengisi array dengan menggunakan looping for
+     for (int i = 0; i < 4; i++)
+     {
+         cout << "Masukan nama ke " << i + 1 << " = ";
+         cin >> nama[i];
+     }
+ 
 
 
 
